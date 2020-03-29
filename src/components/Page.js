ffd4dd67fdb039de5cd445307a13bf544a6cb61f
Brian Duong <brianduongh@gmail.com>
2020-03-29 18:21:26 -0500

    Add Grid items to begin work on adding components

---
+++ src/components/Page.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Grid, Paper, Container } from "@material-ui/core";
+
+const styles = theme => ({
+  content: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+});
+
+function Page(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.content}>
+      <Container>
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={8}>
+            <Paper className={classes.paper}>Main Content</Paper>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Paper className={classes.paper}>Sidebar</Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default withStyles(styles)(Page);
