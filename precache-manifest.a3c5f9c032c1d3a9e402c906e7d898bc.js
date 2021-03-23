c5e8c2b648f23a34e570f1c159a70f0dcf0d8083
v8phan <56612604+v8phan@users.noreply.github.com>
2021-03-23 18:23:37 -0500

    Updates

---
+++ precache-manifest.a3c5f9c032c1d3a9e402c906e7d898bc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0789f0c9c6ae818b3315e8473a16e8a6",
+    "url": "/funkymonkey/index.html"
+  },
+  {
+    "revision": "b8e1f3c5e42dbc6798e5",
+    "url": "/funkymonkey/static/css/2.799c978e.chunk.css"
+  },
+  {
+    "revision": "0b592f07f2c84ddc5d08",
+    "url": "/funkymonkey/static/css/main.ceedd3e6.chunk.css"
+  },
+  {
+    "revision": "b8e1f3c5e42dbc6798e5",
+    "url": "/funkymonkey/static/js/2.77b16da4.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/funkymonkey/static/js/2.77b16da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b592f07f2c84ddc5d08",
+    "url": "/funkymonkey/static/js/main.93d6dbbb.chunk.js"
+  },
+  {
+    "revision": "9f1e13c0676a43e5bee8",
+    "url": "/funkymonkey/static/js/runtime-main.3cdcd254.js"
+  },
+  {
+    "revision": "3cb81002d567006339fd1312e992ac4d",
+    "url": "/funkymonkey/static/media/ccpgif.3cb81002.gif"
+  },
+  {
+    "revision": "5cb5fb947e3073189a6f8395872a89fa",
+    "url": "/funkymonkey/static/media/citypopslide.5cb5fb94.jpg"
+  },
+  {
+    "revision": "2d9eef3ffee7ed54afb9df21d3b6036c",
+    "url": "/funkymonkey/static/media/dgslide.2d9eef3f.jpg"
+  },
+  {
+    "revision": "3d473428c619c684c281d80f6cf1d04b",
+    "url": "/funkymonkey/static/media/mfslide.3d473428.png"
+  },
+  {
+    "revision": "182816fa96b5bb45714eb88e83755474",
+    "url": "/funkymonkey/static/media/mincake.182816fa.jpg"
+  },
+  {
+    "revision": "5e1c3419c02eec5a8c7250debce5cf46",
+    "url": "/funkymonkey/static/media/nightfamily.5e1c3419.jpg"
+  },
+  {
+    "revision": "f73236841e8496c0a156e7c1652f8b20",
+    "url": "/funkymonkey/static/media/redclay.f7323684.jpg"
+  },
+  {
+    "revision": "4be7d995632f52900ca9c4d79cf05236",
+    "url": "/funkymonkey/static/media/timefive.4be7d995.jpg"
+  },
+  {
+    "revision": "4403fe4c27dd0fcc2dde0ab066a3ba19",
+    "url": "/funkymonkey/static/media/weekendfly.4403fe4c.jpg"
+  }
+]);
